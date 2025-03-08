5e30ab2dc7790399fd1ee1205ac09710790db8b8
Harshit Dhundale <harshitdhundale@gmail.com>
2025-03-08 23:04:04 +0530

    redis

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,8 +1,11 @@
-// redisClient.js
 const { createClient } = require('redis');
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL, // e.g., Upstash Redis URL
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: process.env.REDIS_URL.startsWith("rediss://"),
+    rejectUnauthorized: false, // This is often needed with Upstash
+  }
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
